46c2c492c07769f0bd34ffcc70ccc7559fb2434d
yinyu <1195997472@qq.com>
2021-07-09 10:39:01 +0800

    测试f1

---
+++ aaa/src/main/java/controller/nnn.java
@@ -0,0 +1,21 @@
+package controller;
+
+/**
+ * @Auther: yinyu
+ * @Date: 2021/07/09
+ * @Description:
+ */
+public class nnn {
+    /**
+     * 用户Id
+     */
+    private Long userId;
+    /**
+     * 配置键
+     */
+    private String code;
+    /**
+     * 配置值
+     */
+    private String value;
+}
